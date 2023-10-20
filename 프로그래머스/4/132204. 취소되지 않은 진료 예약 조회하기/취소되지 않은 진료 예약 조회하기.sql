bcd950ea332bb7b4545ec1619d4b220b2aa074d3
Taehyun Jimmy Um <43625853+uth0330@users.noreply.github.com>
2023-10-20 18:17:05 +0900

    [level 4] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -0,0 +1,16 @@
+-- 코드를 입력하세요
+SELECT  A.APNT_NO
+        , P.PT_NAME
+        , P.PT_NO
+        , A.MCDP_CD
+        , D.DR_NAME
+        , A.APNT_YMD
+FROM    APPOINTMENT A
+JOIN    DOCTOR D
+    ON  D.DR_ID = A.MDDR_ID
+JOIN    PATIENT P
+    ON  P.PT_NO = A.PT_NO
+WHERE   1 = 1
+    AND DATE_FORMAT(APNT_YMD, "%Y-%m-%d") = "2022-04-13"
+    AND APNT_CNCL_YN = "N"
+ORDER BY    A.APNT_YMD
