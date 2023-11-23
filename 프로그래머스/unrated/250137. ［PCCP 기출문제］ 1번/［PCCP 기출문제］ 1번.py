100e89b9fec4abd106605545f2ff87781cc38671
Taehyun Jimmy Um <43625853+uth0330@users.noreply.github.com>
2023-11-23 18:16:11 +0900

    [level unrated] Title: [PCCP 기출문제] 1번, Time: 0.45 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/250137. ［PCCP 기출문제］ 1번/［PCCP 기출문제］ 1번.py
@@ -0,0 +1,30 @@
+def solution(bandage, health, attacks):
+    answer = 0
+    hp = health
+    bdg_cnt = bandage[0]
+    atk_cnt = 0
+
+    for idx in range(1, attacks[len(attacks) - 1][0] + 1):
+        if attacks[atk_cnt][0] == idx:
+            hp -= attacks[atk_cnt][1]
+            atk_cnt += 1
+            bdg_cnt = bandage[0]
+            if (hp < 1):
+                break
+        else:
+            bdg_cnt -= 1
+            hp += bandage[1]
+            if bdg_cnt == 0:
+                hp += bandage[2]
+                bdg_cnt = bandage[0]
+            if hp > health:
+                hp = health
+        print(hp)
+    print(hp)
+    
+    if hp < 1:
+        answer = -1
+    else:
+        answer = hp
+    
+    return answer
